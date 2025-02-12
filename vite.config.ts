2b376c87e66a859474964f246d2449436d780b68
Tharles Costa <tharlescostadesousa@gmail.com>
2025-02-11 22:08:17 -0300

    - Ajustes no packge.json - vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      outDir: "dist",
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: "src/index.ts",
       name: "ReactInputMasker",
-      fileName: (format) => `react-input-masker.${format}.ts`,
-      formats: ["es", "umd"],
+      fileName: (format) => `react-input-masker.${format}.js`,
+      formats: ["es", "cjs", "umd"],
     },
     rollupOptions: {
       external: ["react", "react-dom"],
